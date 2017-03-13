9de3acc50b87382ad5afa0e20eda123232732015
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-03-13 12:21:00 -0700

    Stretch Header Layout Complete.   Added constraint so that when it stretches it will remain within the bounds of the collectionView

--- StretchyHeaderCollectionViewRW/StretchyHeaderLayout.swift
+++ StretchyHeaderCollectionViewRW/StretchyHeaderLayout.swift
@@ -1,51 +1,77 @@
 //
 //  StretchyHeaderLayout.swift
-//  Character Collector
+//  StretchyHeaderCollectionViewRW
 //
-//  Created by Michael Briscoe on 2/6/17.
-//  Copyright © 2017 Razeware, LLC. All rights reserved.
+//  Created by Cynthia Strickland on 3/8/17.
+//  Copyright © 2017 Fenix Designz. All rights reserved.
 //
 
 import UIKit
 
-class StretchyHeaderLayout: UICollectionViewFlowLayout {
-  override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-    let layoutAttributes = super.layoutAttributesForElements(in: rect)! as [UICollectionViewLayoutAttributes]
+
+class StretchyHeaderAttributes: UICollectionViewLayoutAttributes {
     
-    let inset = collectionView!.contentInset
-    let offset = collectionView!.contentOffset
-    let minY = -inset.top
-    if (offset.y < minY) {
-      let deltaY = fabs(offset.y - minY)
-      for attributes in layoutAttributes {
-        if let elementKind = attributes.representedElementKind {
-          if elementKind == UICollectionElementKindSectionHeader {
-            var frame = attributes.frame
-            frame.size.height = max(0, headerReferenceSize.height + deltaY)
-            frame.origin.y = frame.minY - deltaY
-            attributes.frame = frame
-          }
-        }
-      }
+    var deltaY: CGFloat = 0
+    
+    override func copy(with zone: NSZone?) -> Any {
+        let copy = super.copy(with: zone) as! StretchyHeaderAttributes
+        copy.deltaY = deltaY
+        return copy
     }
     
-    return layoutAttributes
-  }
-
-  override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
-    return true
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    override func isEqual(_ object: Any?) -> Bool {
+        if let attributes = object as? StretchyHeaderAttributes {
+            if attributes.deltaY == deltaY {
+                return super.isEqual(object)
+            }
+        }
+        return false
+    }
 }
+
+class StretchyHeaderLayout: UICollectionViewFlowLayout {
+    
+    var maximumStretchHeight: CGFloat = 0
+    
+    override class var layoutAttributesClass : AnyClass {
+        return StretchyHeaderAttributes.self
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layoutAttributes = super.layoutAttributesForElements(in: rect)! as! [StretchyHeaderAttributes]
+        let offset = collectionView!.contentOffset
+        if (offset.y < 0) {
+            let deltaY = fabs(offset.y)
+            for attributes in layoutAttributes {
+                if let elementKind = attributes.representedElementKind {
+                    if elementKind == UICollectionElementKindSectionHeader {
+                        var frame = attributes.frame
+                        frame.size.height = min(max(0, headerReferenceSize.height + deltaY), maximumStretchHeight)
+                        frame.origin.y = frame.minY - deltaY
+                        attributes.deltaY = deltaY
+                        attributes.frame = frame
+                    }
+                }
+            }
+        }
+        return layoutAttributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
